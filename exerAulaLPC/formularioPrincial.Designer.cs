8d90f015371e95cf68330c6ed13ed58ab5fc89fe
Alves <ramonalvespires@MacBook-Pro-de-Ramon-Alves-Pires.local>
2012-03-29 21:34:34 -0300

    exerAulaLPC

---
+++ exerAulaLPC/formularioPrincial.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace exerAulaLPC
+{
+    partial class formularioPrincial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formularioPrincial));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoFormulárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.utilitáriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculadoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.microsoftWordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleChromeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formulárioPrincipalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.utilitáriosToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(452, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoFormulárioToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoFormulárioToolStripMenuItem
+            // 
+            this.novoFormulárioToolStripMenuItem.Name = "novoFormulárioToolStripMenuItem";
+            this.novoFormulárioToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.novoFormulárioToolStripMenuItem.Text = "Novo Formulário";
+            this.novoFormulárioToolStripMenuItem.Click += new System.EventHandler(this.novoFormulárioToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // utilitáriosToolStripMenuItem
+            // 
+            this.utilitáriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculadoraToolStripMenuItem,
+            this.microsoftWordToolStripMenuItem,
+            this.googleChromeToolStripMenuItem});
+            this.utilitáriosToolStripMenuItem.Name = "utilitáriosToolStripMenuItem";
+            this.utilitáriosToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.utilitáriosToolStripMenuItem.Text = "Utilitários";
+            this.utilitáriosToolStripMenuItem.Click += new System.EventHandler(this.utilitáriosToolStripMenuItem_Click);
+            // 
+            // calculadoraToolStripMenuItem
+            // 
+            this.calculadoraToolStripMenuItem.Name = "calculadoraToolStripMenuItem";
+            this.calculadoraToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.calculadoraToolStripMenuItem.Text = "Calculadora";
+            this.calculadoraToolStripMenuItem.Click += new System.EventHandler(this.calculadoraToolStripMenuItem_Click);
+            // 
+            // microsoftWordToolStripMenuItem
+            // 
+            this.microsoftWordToolStripMenuItem.Name = "microsoftWordToolStripMenuItem";
+            this.microsoftWordToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.microsoftWordToolStripMenuItem.Text = "Bloco de Notas";
+            this.microsoftWordToolStripMenuItem.Click += new System.EventHandler(this.microsoftWordToolStripMenuItem_Click);
+            // 
+            // googleChromeToolStripMenuItem
+            // 
+            this.googleChromeToolStripMenuItem.Name = "googleChromeToolStripMenuItem";
+            this.googleChromeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.googleChromeToolStripMenuItem.Text = "Google Chrome";
+            this.googleChromeToolStripMenuItem.Click += new System.EventHandler(this.googleChromeToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formulárioPrincipalToolStripMenuItem});
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // formulárioPrincipalToolStripMenuItem
+            // 
+            this.formulárioPrincipalToolStripMenuItem.Name = "formulárioPrincipalToolStripMenuItem";
+            this.formulárioPrincipalToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.formulárioPrincipalToolStripMenuItem.Text = "Formulário Principal";
+            this.formulárioPrincipalToolStripMenuItem.Click += new System.EventHandler(this.formulárioPrincipalToolStripMenuItem_Click);
+            // 
+            // formularioPrincial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(452, 318);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "formularioPrincial";
+            this.Text = "Formulario Principal";
+            this.Load += new System.EventHandler(this.formularioPrincial_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoFormulárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem utilitáriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculadoraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem microsoftWordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleChromeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formulárioPrincipalToolStripMenuItem;
+
+    }
+}
+
---
+++ exerAulaLPC/formularioPrincial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace exerAulaLPC
+{
+    public partial class formularioPrincial : Form
+    {
+        public formularioPrincial()
+        {
+            InitializeComponent();
+        }
+
+        private void formularioPrincial_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculadoraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\Windows/System32/Calc.exe");
+        }
+
+        private void utilitáriosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }  
+
+        private void microsoftWordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\Windows/System32/notepad.exe");
+        }
+
+        private void googleChromeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\Users/Ramon Alves/AppData/Local/Google/Chrome/Application/chrome.exe");
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formulárioPrincipalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desenvolvido por Alves, Ramon - Exercício LPC 23/03/2012", "Sobre");
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void novoFormulárioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            novoFormularioPrincipal newNovoFormularioPrincipal = new novoFormularioPrincipal();
+            newNovoFormularioPrincipal.ShowDialog();
+        }
+        }
+    }
